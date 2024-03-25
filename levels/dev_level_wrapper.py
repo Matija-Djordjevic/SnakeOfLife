635cd445aa2912f197b596fd7411d51ce238bc36
Matija Djordjevic <dj.matija.98@gmail.com>
2024-03-25 22:04:51 +0100

    now multi line

--- levels/dev_level_wrapper.py
+++ levels/dev_level_wrapper.py
@@ -5,11 +5,13 @@ from levels.base_level import BaseLevel
 class DevLevelWrapper():
     def __init__(self, level: BaseLevel, clock: pg.time.Clock) -> None:
         self.level = level
-        self._surface = level.surface
         self.clock = clock
+        
         self._display_info = False
-        self._info_font = pg.font.SysFont("Arial", size=45, bold=True)
+        
+        self.font_size = 35
         self.font_color = (255, 255, 255)
+        self._info_font = pg.font.SysFont("Arial", size=self.font_size, bold=False)
     
     def update(self, t_elapsed: float, events: list[pg.event.Event]) -> bool:
         self._handle_events(events)
@@ -22,10 +24,24 @@ class DevLevelWrapper():
             self._do_display_info()
     
     def _do_display_info(self) -> None:
-        fps = self.clock.get_fps()
-        fps = str(int(fps))
-        img = self._info_font.render(fps, True, self.font_color)
-        self._surface.blit(img, (0, 0))
+        ls = self._get_info_list()
+
+        y_off = 0
+        for el in ls:
+            img = self._info_font.render(el, True, self.font_color)
+            self.level.surface.blit(img, (0, y_off))
+            y_off +=  self.font_size
+
+    def _get_info_list(self) -> list[str]:
+        ls = []
+        
+        fps = int(self.clock.get_fps())
+        ls.append(f'{fps} fps')
+        
+        w, h = self.level.surface.get_size()
+        ls.append(f'{w}p X {h}p')
+        
+        return ls
     
     def _handle_events(self, events: list[pg.event.Event]) -> None:
         for event in events:
