6197b680938e480552af57dfbaefa10fc17bed48
Matija Djordjevic <dj.matija.98@gmail.com>
2024-03-31 15:23:46 +0200

    rename

--- levels/base_level.py
+++ levels/base_level.py
@@ -1,11 +1,16 @@
 import pygame as pg
 
+from abc import ABC, abstractmethod
+
+
 class BaseLevel():
     def __init__(self) -> None:
         pass
-    
+
+    @abstractmethod
     def update(self, t_elapsed: float, events: list[pg.event.Event]) -> bool:
-        raise NotImplemented
-    
+        pass
+        
+    @abstractmethod
     def draw(self) -> None:
-        raise NotImplemented
+        pass
